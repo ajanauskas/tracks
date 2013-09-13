03effab79d0ab78b3a6110cd23f5191aca01e29c
Andrius Janauskas <andrius.janauskas@gmail.com>
2013-09-13 12:44:36 +0300

    Extract tag cloud min and max calculation to a method

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -1,44 +1,35 @@
+# tag cloud code inspired by this article
+#  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
+
 class TagCloud
   LEVELS = 10
 
   attr_reader :current_user
   attr_reader :tags_min
-  attr_reader :tags_for_cloud
   attr_reader :tags_divisor
   attr_reader :tags_min_90days
   attr_reader :tags_divisor_90days
-  attr_reader :tags_for_cloud_90days
 
   def initialize(current_user)
-    @current_user    = current_user
+    @current_user = current_user
   end
 
   def compute
-    # tag cloud code inspired by this article
-    #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
+    @tags_min, max               = calculate_min_and_max(tags_for_cloud)
+    @tags_divisor                = ((max - @tags_min) / LEVELS) + 1
+    @tags_min_90days, max_90days = calculate_min_and_max(tags_for_cloud_90days)
+    @tags_divisor_90days         = ((max_90days - @tags_min_90days) / LEVELS) + 1
+  end
 
-    # Get the tag cloud for all tags for actions
-    @tags_for_cloud = get_tags_for_cloud
+  def tags_for_cloud
+    @tags_for_cloud ||= get_tags_for_cloud
+  end
 
-    max, @tags_min = 0, 0
-    @tags_for_cloud.each { |t|
-      max = [t.count.to_i, max].max
-      @tags_min = [t.count.to_i, @tags_min].min
-    }
 
-    @tags_for_cloud_90days = get_tags_for_cloud({
+  def tags_for_cloud_90days
+    @tags_for_cloud_90days ||= get_tags_for_cloud({
       cut_off: 3.months.ago.beginning_of_day
     })
-
-    @tags_divisor = ((max - @tags_min) / LEVELS) + 1
-
-    max_90days, @tags_min_90days = 0, 0
-    @tags_for_cloud_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @tags_min_90days = [t.count.to_i, @tags_min_90days].min
-    }
-
-    @tags_divisor_90days = ((max_90days - @tags_min_90days) / LEVELS) + 1
   end
 
   private
@@ -65,6 +56,15 @@ class TagCloud
     sql_params += [cut_off, cut_off] if cut_off
 
     Tag.find_by_sql(sql_params).sort_by { |tag| tag.name.downcase }
+  end
+
+  def calculate_min_and_max(tags)
+    min, max = 0, 0
+    tags_for_cloud.each { |t|
+      max = [t.count.to_i, max].max
+      min = [t.count.to_i, min].min
+    }
+    return [min, max]
   end
 
 end
